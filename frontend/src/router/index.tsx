aa8669d3657178712e4004c32b72a3124491dc0b
imdavyking <yungdynamic53@gmail.com>
2026-02-21 21:25:50 +0100

    chore: updated

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,21 +1,11 @@
 import { useRoutes } from "react-router-dom";
 import Home from "../views/home/main";
-import NotFound from "../views/not-found/main";
-import NewRoundPage from "../views/new-round/main";
 
 function Router() {
   const routes = [
     {
       path: "/",
       element: <Home />,
-    },
-    {
-      path: "/new-round",
-      element: <NewRoundPage />,
-    },
-    {
-      path: "*",
-      element: <NotFound />,
     },
   ];
   return useRoutes(routes);
