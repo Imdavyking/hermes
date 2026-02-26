111b6d478353006e4317cca22e38fe5981ebdd0a
imdavyking <yungdynamic53@gmail.com>
2026-02-26 12:43:24 +0100

    chore: updated

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot, type Container } from "react-dom/client";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import client from "./services/apollo.services";
+import { ApolloProvider } from "@apollo/client";
 import App from "./App.js";
 
 import { StarknetProvider } from "./providers/StarknetProvider";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root") as Container).render(
   <StrictMode>
-    <StarknetProvider>
-      <App />
-    </StarknetProvider>
-  </StrictMode>
+    <ApolloProvider client={client}>
+      <QueryClientProvider client={queryClient}>
+        <StarknetProvider>
+          <App />
+        </StarknetProvider>
+      </QueryClientProvider>
+    </ApolloProvider>
+  </StrictMode>,
 );
